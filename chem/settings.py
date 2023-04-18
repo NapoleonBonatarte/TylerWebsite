da62d660862209955f8e4accc9413827567c03ee
Tyler Windemuth <twindemuthwork@gmail.com>
2023-04-17 20:30:38 -0700

    update CSRF

--- chem/settings.py
+++ chem/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 
-CSRF_Trusted_ORIGINS = ['https://web-production-566f.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-566f.up.railway.app']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
